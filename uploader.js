ac59f4ca95839e7d81952693ee35ea43554edbc1
Dharmadi Tanamas <dharmadi93@gmail.com>
2016-11-29 16:56:11 +0700

    upload bucket and create new bucket if doesnt have bucket

--- uploader.js
+++ uploader.js
@@ -12,14 +12,44 @@ fs.readFile('./upload.txt', function (err, data) {
         return
     }
 
+
+
     var base64data = new Buffer(data, 'binary')
     var s3 = new AWS.S3()
-    s3.putObject({
-        Bucket: 'dharmadi93',
-        Key: 'berhasil.txt',
-        Body: base64data,
-        ACL: 'public-read'
-    }, function (response) {
-        console.log('uplaoded')
-    })
+
+
+    var bucketsTemp = []
+    var newBucket = 'testingCreateNewBucket'
+    var flag = 0
+    s3.listBuckets(function(err, data) {
+        if (err) console.log(err, err.stack); // an error occurred
+        else {
+            // console.log(data.Buckets);
+            bucketsTemp = data.Buckets
+
+
+            for (var i = 0; i < bucketsTemp.length; i++) {
+                // console.log(bucketsTemp[i].Name)
+                if (bucketsTemp[i].Name === newBucket) {
+                    flag = 1
+                }
+            }
+
+            if (flag === 0) {
+                s3.createBucket({Bucket: newBucket}, function(err, data) {
+                    if (err) console.log(err, err.stack); // an error occurred
+                    else {
+                        s3.putObject({
+                            Bucket: newBucket,
+                            Key: 'berhasil.txt',
+                            Body: base64data,
+                            ACL: 'public-read'
+                        }, function (response) {
+                            console.log('uploaded')
+                        })
+                    }
+                });
+            }
+        }           // successful response
+    });
 })
